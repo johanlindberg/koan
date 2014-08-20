8750e215184c7913d6d27a6c03821ebee1ee4be1
Johan Lindberg <johan@pulp.se>
2014-08-20 21:28:16 +0200

    Boiler plate code to start reading the file.

--- sling-blade-runner/node/aug2014.js
+++ sling-blade-runner/node/aug2014.js
@@ -6,5 +6,21 @@ var LBL = require('line-by-line');
 solve('../MOVIES.TEST');
 
 function solve(file) {
-    console.log(file);
+    xref = make_xref(file);
+}
+
+function make_xref(file) {
+    var fin = new LBL(file);
+    var line_no = 0;
+
+    fin.on('line',
+	   function(line) {
+	       console.log(line);
+	       line_no++;
+	   });
+
+    fin.on('end',
+	   function() {
+	       console.log(line_no + " titles read");
+	   }); 
 }
